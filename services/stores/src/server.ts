ee5cc2e771413e6660952c54ebcb5801f06a0eb7
Leonardo A <leo.adriano1994@gmail.com>
2022-12-24 04:07:56 -0300

    fix[#1]: change server.ts folder

--- services/stores/src/server.ts
+++ services/stores/src/server.ts
@@ -1,6 +1,8 @@
 import App from '@infrastructure/http'
 import Logger from '@marketplace/logger'
 
+process.env.SERVICE_NAME = 'stores'
+
 const port = process.env.PORT ?? 3000
 
 App.listen(port, () => {
